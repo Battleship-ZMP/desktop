ec63690cdb024ed78737319cf75ba3a1a1f42cc4
DanielHajduk98 <DanHajduk@protonmail.com>
2020-03-20 19:20:26 +0100

    Connected Firebase
    Firebase config is stored in firebaseConfig.js. Api key is stored in env file. Devtools are available only in dev env

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import React, { Component } from "react";
 import "./App.css";
 import { Provider } from "react-redux";
-import store from "./store";
+import store from "./store/store";
 import Routes from "./routes";
 import { BrowserRouter as Router } from "react-router-dom";
 
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import { ReactReduxFirebaseProvider } from "react-redux-firebase";
+import { fbConfig, rrfProps } from "./firebaseConfig";
+
 import Navbar from "./components/Navbar";
+
+firebase.initializeApp(fbConfig);
+firebase.firestore();
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Router>
-          <div className="App">
-            <Navbar />
-            <Routes />
-          </div>
-        </Router>
+        <ReactReduxFirebaseProvider {...rrfProps}>
+          <Router>
+            <div className="App">
+              <Navbar />
+              <Routes />
+            </div>
+          </Router>
+        </ReactReduxFirebaseProvider>
       </Provider>
     );
   }
---
+++ src/firebaseConfig.js
@@ -0,0 +1,26 @@
+import firebase from "firebase/app";
+import store from "./store/store";
+import { createFirestoreInstance } from "redux-firestore";
+
+export const fbConfig = {
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: "coolrecipes-f4e21.firebaseapp.com",
+  databaseURL: "https://coolrecipes-f4e21.firebaseio.com",
+  projectId: "coolrecipes-f4e21",
+  storageBucket: "coolrecipes-f4e21.appspot.com",
+  messagingSenderId: "387025921582",
+  appId: "1:387025921582:web:bffc515356a4005b79a1bf",
+  measurementId: "G-4BK3297DFV"
+};
+
+const rrfConfig = {
+  userProfile: "users",
+  useFirestoreForProfile: true
+};
+
+export const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance
+};
