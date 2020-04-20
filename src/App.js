6b0e5520e93c008d373db938b9b9cbd8b9aba6b7
DanielHajduk98 <danhajduk@protonmail.com>
2020-04-20 18:59:10 +0200

    Implemented MDBReact

--- src/App.js
+++ src/App.js
@@ -9,8 +9,9 @@ import "firebase/firestore";
 import { ReactReduxFirebaseProvider } from "react-redux-firebase";
 import { fbConfig, rrfProps } from "./firebaseConfig";
 
-import "./utils/globalStyle.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
 
 import Main from "./components/Main/Main";
 
