0251977c9499793486948d62f306a68cbb859c75
DanielHajduk98 <danhajduk@protonmail.com>
2020-04-24 17:13:13 +0200

    Form validation

--- src/components/Recipe/Editor.js
+++ src/components/Recipe/Editor.js
@@ -2,15 +2,28 @@ import React, { Component } from "react";
 import {
   MDBBtn,
   MDBCard,
-  MDBCardBody, MDBCardFooter,
+  MDBCardBody,
+  MDBCardFooter,
   MDBCardHeader,
   MDBCardTitle,
   MDBCol,
-  MDBContainer, MDBFooter,
+  MDBContainer,
+  MDBFooter,
   MDBIcon,
   MDBInput,
-  MDBRow
+  MDBModal,
+  MDBRow,
 } from "mdbreact";
+import * as Yup from "yup";
+import { Formik, Field, Form } from "formik";
+
+const AddRecipeSchema = Yup.object().shape({
+  name: Yup.string().required("Tytuł jest wymagany"),
+  description: Yup.string(),
+  ingredients: Yup.string().required("Podanie składników jest wymagane"),
+  instructions: Yup.string().required("Instrukcje przepisu są wymagane"),
+  photo: Yup.string(),
+});
 
 class Editor extends Component {
   render() {
@@ -20,25 +33,124 @@ class Editor extends Component {
           <MDBCardHeader color="teal" className="teal">
             <MDBCardTitle className="text-white">Dodaj przepis!</MDBCardTitle>
           </MDBCardHeader>
-          <MDBCardBody className="">
-            <form>
-              <MDBRow>
-                <MDBCol md="8">
-                  <MDBInput label="Tytuł"/>
-                  <MDBInput label="Opis"/>
-                  <MDBInput label="Składniki"/>
-                  <MDBInput label="Przepis"/>
-                </MDBCol>
-                <MDBCol md="4">
-                  {/*TODO adding photo */}
-                  <MDBInput label="Zdjęcie" icon="camera"/>
-                </MDBCol>
-              </MDBRow>
-            </form>
-          </MDBCardBody>
-          <MDBCardFooter>
-            <MDBBtn color="teal" className="text-white">Wyślij</MDBBtn>
-          </MDBCardFooter>
+          <Formik
+            initialValues={{
+              name: "",
+              description: "",
+              ingredients: "",
+              instructions: "",
+              photo: "",
+            }}
+            isInitialValid={false}
+            validationSchema={AddRecipeSchema}
+            onSubmit={(values, { setSubmitting }) => {
+              console.log(values);
+            }}
+          >
+            {({ isSubmitting, isValid }) => (
+              <Form>
+                <MDBCardBody>
+                  <MDBRow>
+                    <MDBCol md="8">
+                      <Field name="name">
+                        {({ field, form: { touched, errors }, meta }) => (
+                          <div>
+                            <MDBInput
+                              containerClass="mb-0 pb-0"
+                              type="text"
+                              label="Tytuł"
+                              {...field}
+                            />
+                            {meta.touched && meta.error && (
+                              <small className="text-danger m-0 p-0">
+                                {meta.error}
+                              </small>
+                            )}
+                          </div>
+                        )}
+                      </Field>
+                      <Field name="description">
+                        {({ field }) => (
+                          <div>
+                            <MDBInput
+                              containerClass="mb-0 pb-0"
+                              type="text"
+                              label="Opis"
+                              {...field}
+                            />
+                          </div>
+                        )}
+                      </Field>
+                      <Field name="ingredients">
+                        {({ field, form: { touched, errors }, meta }) => (
+                          <div>
+                            <MDBInput
+                              containerClass="mb-0 pb-0"
+                              type="text"
+                              label="Składniki"
+                              {...field}
+                            />
+                            {meta.touched && meta.error && (
+                              <small className="text-danger m-0 p-0">
+                                {meta.error}
+                              </small>
+                            )}
+                          </div>
+                        )}
+                      </Field>
+                      <Field name="instructions">
+                        {({ field, form: { touched, errors }, meta }) => (
+                          <div>
+                            <MDBInput
+                              containerClass="mb-0 pb-0"
+                              type="text"
+                              label="Instrukcje"
+                              {...field}
+                            />
+                            {meta.touched && meta.error && (
+                              <small className="text-danger m-0 p-0">
+                                {meta.error}
+                              </small>
+                            )}
+                          </div>
+                        )}
+                      </Field>
+                    </MDBCol>
+                    <MDBCol md="4">
+                      {/*TODO adding photo */}
+                      <Field name="photo">
+                        {({ field, form: { touched, errors }, meta }) => (
+                          <div>
+                            <MDBInput
+                              containerClass="mb-0 pb-0"
+                              type="text"
+                              label="Zdjęcie"
+                              {...field}
+                            />
+                            {meta.touched && meta.error && (
+                              <small className="text-danger m-0 p-0">
+                                {meta.error}
+                              </small>
+                            )}
+                          </div>
+                        )}
+                      </Field>
+                    </MDBCol>
+                  </MDBRow>
+                </MDBCardBody>
+                <MDBCardFooter>
+                  <MDBBtn
+                    type="submit"
+                    disabled={!isValid}
+                    color="teal"
+                    className="text-white"
+                  >
+                    Wyślij
+                  </MDBBtn>
+                </MDBCardFooter>
+              </Form>
+            )}
+          </Formik>
         </MDBCard>
       </MDBContainer>
     );
